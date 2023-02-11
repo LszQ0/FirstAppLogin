cf38ad072abdfbb1903b0632384914358c88cd31
LszQ0 <michallech94@gmail.com>
2023-02-11 16:35:03 +0100

    Revert "Initial commit"
    This reverts commit 617834b9

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.firstapp.firstapplogin {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.firstapp.firstapplogin to javafx.fxml;
-    exports com.firstapp.firstapplogin;
-}
